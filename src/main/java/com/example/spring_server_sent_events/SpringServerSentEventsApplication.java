a91b26690aafcff7355dd6b7987656c93eceff6d
Esmaeil Chitgar <ec.chitgar@gmail.com>
2024-09-11 09:43:01 +0330

    initilize

---
+++ src/main/java/com/example/spring_server_sent_events/SpringServerSentEventsApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring_server_sent_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// from https://medium.com/globant/server-sent-events-in-spring-boot-6a2c512a5475
+@SpringBootApplication
+public class SpringServerSentEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerSentEventsApplication.class, args);
+	}
+
+}
